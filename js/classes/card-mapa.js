862d705a2433bbf98ef4889e9f2e8465f7c971b1
LUIZ GUSTAVO DA SILVA <22282203@CFP127ALUNO.LOCAL>
2023-05-08 15:06:33 -0300

    Responsividade finalizada

--- js/classes/card-mapa.js
+++ js/classes/card-mapa.js
@@ -55,29 +55,35 @@ class card_mapa extends HTMLElement {
         .card__mapa {
             display: flex;
             flex-direction: column;
-            gap: 1.25rem;
+            gap: clamp(0.75rem, 0.61rem + 0.5333vw, 1.25rem);
             background-color: white;
             align-items: center;
-            height: 525px;
-            width: 380px;
+            height: clamp(24.0625rem, 21.6125rem + 9.3333vw, 32.8125rem);
+            width: clamp(16.5625rem, 14.55rem + 7.6667vw, 23.75rem);
             border-radius: 1.5rem;
+            transition: all 0.5s;
+        }
+
+        .card__mapa:hover{
+            width: clamp(18.125rem, 16.1125rem + 7.6667vw, 25.3125rem);
+            height: clamp(25.625rem, 23.175rem + 9.3333vw, 34.375rem);
         }
         
         .nome__mapa {
             font-family: 'Poppins';
             font-style: normal;
             font-weight: 700;
-            font-size: 1.5rem;
+            font-size: clamp(1.25rem, 1.18rem + 0.2667vw, 1.5rem);
         }
         
         .foto__mapa {
-            width: 295px;
-            height: 202px;
+            width: clamp(12.5rem, 10.8375rem + 6.3333vw, 18.4375rem);
+            height: clamp(8.4375rem, 7.3rem + 4.3333vw, 12.5rem);
         }
         
         .mini__mapa {
-            width: 256px;
-            height: 224px;
+            width: clamp(10.9375rem, 9.52rem + 5.4vw, 16rem);   
+            height: clamp(11.25rem, 10.4625rem + 3vw, 14.0625rem);
         }
         `
         return css
